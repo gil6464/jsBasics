ddd05d8937f23b6aa44fa73bbab5f2d76d84230e
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-18 18:57:33 +0200

    qs 2 solve

---
+++ qs2.js
@@ -0,0 +1,8 @@
+let number1= prompt("Enter hare first number");
+let number2= prompt("Enter hare second number");
+let number3= prompt("Enter hare third number");
+if (number1*number2*number3> 0) {
+     alert("+")
+}else{
+     alert("-")
+}
