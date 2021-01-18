f11fb82c5e4e5af295970b97a0c7b2baa3c00dc8
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-18 18:39:59 +0200

    qs1 solve

---
+++ qs1.js
@@ -0,0 +1,9 @@
+let num1= prompt("Enter hare first number");
+let num2= prompt("Enter hare second number");
+if (num1 > num2) {
+   alert(num1  +" Is bigger then " + num2)
+}else if (num2 > num1){
+     alert(num2 + " Is bigger then " + num1) 
+}else {
+     alert("The numbers are equal!")
+}
